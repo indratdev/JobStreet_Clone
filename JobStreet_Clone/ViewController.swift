111dff4e2e62fc780b5bcd7cfdc627b13aeb0c11
Indratdev <indrat.dev@gmail.com>
2020-10-17 15:20:01 +0700

    Initial Commit

---
+++ JobStreet_Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JobStreet_Clone
+//
+//  Created by IndratS on 17/10/20.
+//  Copyright © 2020 IndratSaputra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
